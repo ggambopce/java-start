ba96ee5878ae7dce6981fe9209a67df6367196a7
ggambopce <ggambopce@gmail.com>
2025-06-03 07:35:34 +0900

    ✨ 자바 프로그램의 시작

---
+++ src/main/java/Hellojava.java
@@ -0,0 +1,27 @@
+public class Hellojava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
+/* public class HelloJava
+* 자바 언어는 대소문자를 구분한다. 대소문자가 다르면 오류가 발생할 수 있다.
+* HelloJava를 클래스라 한다.
+* 파일명과 클래스 이름이 같아야 한다.
+* {} 블록을 사용해서 클래스의 시작과 끝을 나타낸다.
+* */
+
+/* public static void main(String[] args)
+* main` 메서드라 한다.
+* 자바는 `main(String[] args)` 메서드를 찾아서 프로그램을 시작한다.
+* */
+
+/* System.out.println("hello java");`
+* System.out.println() : 값을 콘솔에 출력하는 기능이다.
+* "hello java" : 자바는 문자열을 사용할 때 "(쌍따옴표)를 사용한다. 쌍따옴표 사이에 원하는 문자열을 감싸면 된다.
+* `;` : 자바는 세미콜론으로 문장을 구분한다. 문장이 끝나면 세미콜론을 필수로 넣어주어야 한다.
+* */
+
+/*
+* 들여쓰기는 보통 스페이스 4번을 사용한다.
+* 프로그램은 `main()` 을 시작으로 위에서 아래로 한 줄 씩 실행된다.
+* */
