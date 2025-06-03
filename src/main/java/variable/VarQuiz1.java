0c4a9395bd57075bf2f3b17b67fc9605bffa5ed0
ggambopce <ggambopce@gmail.com>
2025-06-04 08:09:36 +0900

    ✨ 변수 문제

--- src/main/java/variable/VarQuiz1.java
+++ src/main/java/variable/VarQuiz1.java
@@ -1,4 +1,22 @@
 package variable;
 
+/** 문제 1
+ * 다음 코드에 반복해서 나오는 숫자 `4` ,`3` 을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 `num1` , `num2` 를 사용하도록 변경해보세요.
+ public class VarEx1Question {
+    public static void main(String[] args) {
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+    }
+ }
+ */
+
 public class VarQuiz1 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
 }
