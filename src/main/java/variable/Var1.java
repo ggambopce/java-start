e9d502e6e92f78dd134d9bb1e57e150db910849a
ggambopce <ggambopce@gmail.com>
2025-06-04 06:45:06 +0900

    ✨ 변수 선언 초기화

--- src/main/java/variable/Var1.java
+++ src/main/java/variable/Var1.java
@@ -9,7 +9,7 @@ public class Var1 {
     }
 }
 
-/* 패키지(package)
+/** 패키지(package)
 * variable` 라는 패키지를 만들었다면 소속된 패키지를 선언해주어야 한다.
 * 자바 파일이 위치하는 패키지와 `package variable` 선언 위치가 같아야 한다.
 * */
---
+++ src/main/java/variable/Var3.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
