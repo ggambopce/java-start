c08368916a0c55eec4dfa4d696900c2af52148ce
ggambopce <ggambopce@gmail.com>
2025-06-03 09:23:20 +0900

    ✨ 변수

---
+++ src/main/java/variable/Var1.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var1 {
+
+    public static void main(String[] arg) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
+
+/* 패키지(package)
+* variable` 라는 패키지를 만들었다면 소속된 패키지를 선언해주어야 한다.
+* 자바 파일이 위치하는 패키지와 `package variable` 선언 위치가 같아야 한다.
+* */
